26c8bd72e1fb300df877ba5705379824deea2a0c
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2025-02-05 00:09:10 +0000

    creating the project

--- hexagonal/src/main/java/com/example/hexagonal/HexagonalApplication.java
+++ hexagonal/src/main/java/com/example/hexagonal/HexagonalApplication.java
@@ -1,4 +1,4 @@
-package com.example.hexagonal.hexagonal;
+package com.example.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hexagonal/src/test/java/com/example/hexagonal/HexagonalApplicationTests.java
+++ hexagonal/src/test/java/com/example/hexagonal/HexagonalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hexagonal.hexagonal;
+package com.example.hexagonal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
