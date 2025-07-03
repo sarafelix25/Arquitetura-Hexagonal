4feb8eae8f9f150f92605a122867eb62b129c1b8
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2025-07-03 18:17:30 +0000

    creating client for find customer

---
+++ hexagonal/src/main/java/com/example/hexagonal/adapters/out/client/response/FindAddressByZipCodeClient.java
@@ -0,0 +1,18 @@
+package com.example.hexagonal.adapters.out.client;
+
+import com.example.hexagonal.adapters.out.client.response.AddressResponse;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+    name = "FindAddressByZipCodeClient",
+    url = "${sara.client.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find(@PathVariable("zipCode") String zipCode);
+git
+}
