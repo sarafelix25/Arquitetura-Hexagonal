1f9a1dfbe6514c906c614bfdebf5c4b1a6cb6916
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2025-07-03 15:19:54 -0300

    Creating Client (#4)
    * creating usecase implementing new methods
    
    * creating client for find customer

--- hexagonal/src/main/java/com/example/hexagonal/HexagonalApplication.java
+++ hexagonal/src/main/java/com/example/hexagonal/HexagonalApplication.java
@@ -1,8 +1,10 @@
 package com.example.hexagonal;
 
+import org.springframework.cloud.openfeign.EnableFeignClient; 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClient
 @SpringBootApplication
 public class HexagonalApplication {
 
---
+++ hexagonal/src/main/java/com/example/hexagonal/adapters/out/client/response/AddressResponse.java
@@ -0,0 +1,11 @@
+package com.example.hexagonal.adapters.out.client;
+
+import lombok.Data;
+
+@Data
+public class AddressResponse {
+
+    private String street;
+    private String city;
+    private String state;
+}
